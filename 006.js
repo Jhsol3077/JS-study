1255e3a67231534e1814f5723de3af0edc783782
hansol <waw8805@hanmail.net>
2024-03-24 15:07:33 +0900

    my first commit

---
+++ 006.js
@@ -0,0 +1,23 @@
+let myName = '제이펍';
+//let은 변수 선언에 사용되므로 다른 값으로 값 변경이 가능
+myName = '고래';
+alert(myName);
+
+let myString='제이펍';
+let currentDate=new Date();
+let myFunction = () => console.log('함수 확인!');
+
+let number1=10;
+let number2=20;
+let sum = number1 + number2;
+console.log(sum); //결과값은 30
+
+let firstName='브렌던';
+let familyName='아이크';
+let fullName = firstName + familyName;
+console.log(fullName); //결과값은 브렌던아이크
+
+let value1=100;
+let value2= value1;
+console.log(value2); //결과값은 100으로 value1과 같은 값, 변수를 다른 변수에 대입하는 것이 가능
+
---
+++ 007.js
@@ -0,0 +1,5 @@
+const myName = '사자';
+alert(myName);
+
+const myString = '사자';
+const myFunction = () => console.log('함수 확인');
